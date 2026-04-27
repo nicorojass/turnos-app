22487479523e0be6db7ae6ae7c7e4354fcdd36c5
Nicolas Rojas <nicoorojas1@gmail.com>
2026-04-27 18:40:24 -0300

    feat: Business entity created

--- src/main/java/com/grupo8/turnos_app/modules/business/Business.java
+++ src/main/java/com/grupo8/turnos_app/modules/business/Business.java
@@ -1,5 +1,59 @@
 package com.grupo8.turnos_app.modules.business;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "businesses")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Business {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String slug;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column
+    private String description;
+
+    private Boolean automaticSchedule;
+
+    private LocalDateTime scheduleEnd;
+
+    private Integer scheduleDaysToCreate;
+
+    private Integer scheduleAnticipation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private User owner;
+
 }
