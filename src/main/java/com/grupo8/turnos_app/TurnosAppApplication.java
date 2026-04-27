38a3472fd4968210cec959312ad1b24800ec9b32
Nicolas Rojas <nicoorojas1@gmail.com>
2026-04-27 11:37:36 -0300

    first commit: initial project setup

---
+++ src/main/java/com/grupo8/turnos_app/TurnosAppApplication.java
@@ -0,0 +1,13 @@
+package com.grupo8.turnos_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo8/turnos_app/TurnosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo8.turnos_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurnosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
