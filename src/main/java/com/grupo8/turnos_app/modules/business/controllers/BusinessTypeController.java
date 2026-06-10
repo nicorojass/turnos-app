1209412dbe6dbd3e48d9a90d3fe99e81bf9c9acb
Nicolas Rojas <nicoorojas1@gmail.com>
2026-06-10 11:39:48 -0300

    refactor: change PatchMapping to Delete for BusinessType

--- src/main/java/com/grupo8/turnos_app/modules/business/controllers/BusinessTypeController.java
+++ src/main/java/com/grupo8/turnos_app/modules/business/controllers/BusinessTypeController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -42,7 +42,7 @@ public class BusinessTypeController {
         return ResponseEntity.ok(businessTypeService.updateBusinessType(id, request));
     }
 
-    @PatchMapping("/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> toggleBusinessTypeActive(@PathVariable Long id) {
         businessTypeService.toggleBusinessTypeActive(id);
         return ResponseEntity.noContent().build();
