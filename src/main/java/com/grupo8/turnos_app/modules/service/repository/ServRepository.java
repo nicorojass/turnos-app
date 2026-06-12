fb6a9a719848e7640495180a48f4bcfce32cec89
irianni-nicolas <fenchi19@gmail.com>
2026-06-12 13:11:06 -0300

    Update ServRepository.java

--- src/main/java/com/grupo8/turnos_app/modules/service/repository/ServRepository.java
+++ src/main/java/com/grupo8/turnos_app/modules/service/repository/ServRepository.java
@@ -3,11 +3,10 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.grupo8.turnos_app.modules.service.entity.Serv;
 
-@Repository
+
 public interface ServRepository extends JpaRepository<Serv, Long> {
     
     List<Serv> findByBusinessId(Long businessId);
